116f8663fdb4b04b864cacc09c752a21cc14e7dc
ahincho <ahincho@unsa.edu.pe>
2024-06-10 10:27:08 -0500

    Refactor Folders and Query from Repository

--- CodeDonor/src/tasks/tasks.persistence.controller.ts
+++ CodeDonor/src/tasks/tasks.persistence.controller.ts
@@ -1,15 +1,23 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksPersistenceService } from './tasks.persistence.service';
 import { TaskEntity } from './persistence/task.entity';
 import { TaskCreateDto } from './dtos/task.create.dto';
 import { TaskUpdateDto } from './dtos/task.update.dto';
+import { Status } from './tasks.memory.models';
+import { StatusPipe } from './pipes/status/status.pipe';
+import { TaskSearchDto } from './dtos/task.search.dto';
+import { SearchPipe } from './pipes/search/search.pipe';
 
 @Controller('/api/v2/tasks')
 export class TasksPersistenceController {
   constructor (private readonly tasksPersistenceService: TasksPersistenceService) { }
   @Get()
-  public getTasks(): Promise<TaskEntity[]> {
-    return this.tasksPersistenceService.getAllTasks();
+  public getTasks(@Query(ValidationPipe, SearchPipe) taskSearchDto: TaskSearchDto): Promise<TaskEntity[]> {
+    if (Object.keys(taskSearchDto).length) {
+      return this.tasksPersistenceService.getTasksWithFilters(taskSearchDto);
+    } else {
+      return this.tasksPersistenceService.getAllTasks();
+    }
   }
   @Get(':id')
   public getTaskById(@Param('id', ParseIntPipe) id: number): Promise<TaskEntity> {
@@ -26,11 +34,11 @@ export class TasksPersistenceController {
     this.tasksPersistenceService.updateTaskById(id, taskUpdateDto);
   }
   @Patch(':id/status')
-  public updateTaskStatusById(@Param('id', ParseIntPipe) id: number) {
-    
+  public updateTaskStatusById(@Param('id', ParseIntPipe) id: number, @Body('status', StatusPipe) status: Status) {
+    this.tasksPersistenceService.updateTaskStatusById(id, status);
   }
   @Delete(':id')
   public deleteTaskById(@Param('id', ParseIntPipe) id: number) {
-    
+    this.tasksPersistenceService.deleteTaskById(id);
   }
 }
--- CodeDonor/src/tasks/tasks.persistence.service.ts
+++ CodeDonor/src/tasks/tasks.persistence.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskEntity } from './persistence/task.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { TaskCreateDto } from './dtos/task.create.dto';
 import { Status } from './tasks.memory.models';
 import { TaskUpdateDto } from './dtos/task.update.dto';
+import { TaskSearchDto } from './dtos/task.search.dto';
 
 @Injectable()
 export class TasksPersistenceService {
@@ -12,8 +13,19 @@ export class TasksPersistenceService {
     @InjectRepository(TaskEntity)
     private readonly taskRepository: Repository<TaskEntity>
   ) { }
-  public async getAllTasks() {
+  public async getAllTasks(): Promise<TaskEntity[]> {
     return this.taskRepository.find();
+  }
+  public async getTasksWithFilters(taskSearchDto: TaskSearchDto): Promise<TaskEntity[]> {
+    const { status, search } = taskSearchDto;
+    const conditions: any = {};
+    if (status) {
+      conditions.status = status;
+    }
+    if (search) {
+      conditions.title = Like(`%${search}%`);
+    }
+    return this.taskRepository.findBy(conditions);
   }
   public async getTaskById(id: number): Promise<TaskEntity> {
     const task = await this.taskRepository.findOneBy({ id });
@@ -41,5 +53,20 @@ export class TasksPersistenceService {
     task.description = description;
     task.status = status;
     await this.taskRepository.update(id, task);
+  }
+  public async updateTaskStatusById(id: number, status: Status): Promise<void> {
+    const task = await this.taskRepository.findOneBy({ id });
+    if (!task) {
+      throw new NotFoundException(`Task with the id = ${id} does not exists!`);
+    }
+    task.status = status;
+    await this.taskRepository.update(id, task);
+  }
+  public async deleteTaskById(id: number): Promise<void> {
+    const task = await this.taskRepository.findOneBy({ id });
+    if (!task) {
+      throw new NotFoundException(`Task with the id = ${id} does not exists!`);
+    }
+    await this.taskRepository.delete({ id });
   }
 }
